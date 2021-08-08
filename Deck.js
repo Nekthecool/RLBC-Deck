451bcac8ce2dac7d4476fc5be25b802b6d87e29e
Nekthecool <nekthecool@gmail.com>
2021-08-08 15:59:58 +0300

    ok

---
+++ Deck.js
@@ -0,0 +1,57 @@
+
+
+function swapImage1() {
+    var image = document.getElementById("imageToSwap1");
+    var dropd = document.getElementById("card1");
+    image.src = dropd.value;
+  }
+
+  function swapImage2() {
+    var image = document.getElementById("imageToSwap2");
+    var dropd = document.getElementById("card2");
+    image.src = dropd.value;
+  }
+
+  function swapImage3() {
+    var image = document.getElementById("imageToSwap3");
+    var dropd = document.getElementById("card3");
+    image.src = dropd.value;
+  }
+
+  function swapImage4() {
+    var image = document.getElementById("imageToSwap4");
+    var dropd = document.getElementById("card4");
+    image.src = dropd.value;
+  }
+
+  function swapImage5() {
+    var image = document.getElementById("imageToSwap5");
+    var dropd = document.getElementById("card5");
+    image.src = dropd.value;
+  }
+
+  function swapImage6() {
+    var image = document.getElementById("imageToSwap6");
+    var dropd = document.getElementById("card6");
+    image.src = dropd.value;
+  }
+
+  function swapImage7() {
+    var image = document.getElementById("imageToSwap7");
+    var dropd = document.getElementById("card7");
+    image.src = dropd.value;
+  }
+
+  function swapImage8() {
+    var image = document.getElementById("imageToSwap8");
+    var dropd = document.getElementById("card8");
+    image.src = dropd.value;
+  }
+
+  function swapImage9() {
+    var image = document.getElementById("imageToSwap9");
+    var dropd = document.getElementById("card9");
+    image.src = dropd.value;
+  }
+
+  
