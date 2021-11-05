b3e58d52bcff016a3dadbe566cf51191073ef76e
Nekthecool <88107692+Nekthecool@users.noreply.github.com>
2021-11-05 22:36:20 +0200

    save deck

--- Deck.js
+++ Deck.js
@@ -1,4 +1,5 @@
-
+document.getElementById('SaveDeck').addEventListener('click',save);
+document.getElementById('LoadDeck').addEventListener('click',load);
 
 function swapImage1() {
     var image = document.getElementById("imageToSwap1");
@@ -54,4 +55,38 @@ function swapImage1() {
     image.src = dropd.value;
   }
 
+  function save() {
+    var image_1 = document.getElementById("imageToSwap1").src;
+    var image_2 = document.getElementById("imageToSwap2").src;
+    var image_3 = document.getElementById("imageToSwap3").src;
+    var image_4 = document.getElementById("imageToSwap4").src;
+    var image_5 = document.getElementById("imageToSwap5").src;
+    var image_6 = document.getElementById("imageToSwap6").src;
+    var image_7 = document.getElementById("imageToSwap7").src;
+    var image_8 = document.getElementById("imageToSwap8").src;
+    var image_9 = document.getElementById("imageToSwap9").src;
+    
+    localStorage.setItem('image1', image_1);
+    localStorage.setItem('image2', image_2);
+    localStorage.setItem('image3', image_3);
+    localStorage.setItem('image4', image_4);
+    localStorage.setItem('image5', image_5);
+    localStorage.setItem('image6', image_6);
+    localStorage.setItem('image7', image_7);
+    localStorage.setItem('image8', image_8);
+    localStorage.setItem('image9', image_9);
+    alert("Deck successfully saved!");
+  } 
+
   
+ function load(){
+    document.getElementById("imageToSwap1").src = localStorage.getItem('image1')
+    document.getElementById("imageToSwap2").src = localStorage.getItem('image2')
+    document.getElementById("imageToSwap3").src = localStorage.getItem('image3')
+    document.getElementById("imageToSwap4").src = localStorage.getItem('image4')
+    document.getElementById("imageToSwap5").src = localStorage.getItem('image5')
+    document.getElementById("imageToSwap6").src = localStorage.getItem('image6')
+    document.getElementById("imageToSwap7").src = localStorage.getItem('image7')
+    document.getElementById("imageToSwap8").src = localStorage.getItem('image8')
+    document.getElementById("imageToSwap9").src = localStorage.getItem('image9')
+  }
